7326d6c1120e352dddc63195aab7df432a1039ea
Teis Laptop <teisjohannesen@gmail.com>
2018-02-07 15:59:08 +0100

    Dance till your dead update

--- Mini_ex1/empty-example/sketch.js
+++ Mini_ex1/empty-example/sketch.js
@@ -1,10 +1,17 @@
 function setup() {
-  // put setup code here
+  createCanvas(500, 500);
+  background(200);
+  image = loadImage("Dog.gif");
 }
 
 function draw() {
-  ellipse(50,50,100,100);
-  rect(100,100,55,55);
+  fill(20, 20, 255);
+  rect(325,400,150,50,20);
+  //Rect.x-coordinates, y-coordinates, lenght, height, corners
 
-  //Test
+  fill(255, 255, 255);
+  textSize(32);
+  text('Upgrade', 335, 435);
+
+  image(img, 50, 50);
 }
