d3d5e5822ff71cd9b76cf4c92b276e8483674cbd
Teis Laptop <teisjohannesen@gmail.com>
2018-02-16 14:01:40 +0100

    Isaac 1.2
    Added Mom's knife. Added, rotated, flipped and placed images to match WASD controls.

--- Mini_ex2/empty-example/sketch.js
+++ Mini_ex2/empty-example/sketch.js
@@ -2,6 +2,10 @@ var level;
   //Level bagground
 var isaac;
   //Isaac character
+var knife;
+  //This is Mom's knife. My temporary 'projectile'
+var knifescale = 5;
+  //The scaling of the knife. Lower means bigger.
 var x = 0;
   //movement in x-axis
 var y = 0;
@@ -14,13 +18,16 @@ var Isaacx;
   //Isaac's x position
 var Isaacy;
   //Isaac's y position
+var Bulletdirection;
+  //Bulletdirection, so far in degrees
 //In the above text i declare my variabels for the program
 
 function preload() {
-level = loadImage('Assets/level.png')
+  level = loadImage('Assets/level.png')
   //This is the levels image running in the bagground.
   //Without doors so far.
-isaac = loadImage('Assets/Isaac.png')
+  isaac = loadImage('Assets/Isaac.png')
+  knife = loadImage('Assets/Knife.png')
   //this is the picture of Isaac, our primary
   //character. This is the pixelated version.
   }
@@ -38,7 +45,6 @@ function setup() {
   frameRate(FramesPS)
   imageMode(CORNER)
   background(level);
-
   }
 
 function draw() {
@@ -49,11 +55,12 @@ function draw() {
     //Defining Isaac's x position in math
   Isaacy = height/2 + y;
     //Defining Isaac's y position in math
+
   image(isaac, Isaacx, Isaacy, 150, 150 );
 
-  if (keyIsDown(DOWN_ARROW)) {
-    y += movementSpeed;
-    }
+    if (keyIsDown(DOWN_ARROW)) {
+      y += movementSpeed;
+      }
     if (keyIsDown(UP_ARROW)) {
       y -= movementSpeed;
       }
@@ -63,12 +70,41 @@ function draw() {
     if (keyIsDown(RIGHT_ARROW)) {
       x += movementSpeed;
       }
-
-
-}
-
-
-function mousePressed() {
-// Here i want to make W, A, S and D able to 'shoot'
-// projectiles in their respective directions
+    if (keyIsDown(65)) { //a + description
+      push();
+      imageMode(CENTER)
+        //Center the picture on the reference point
+      translate(Isaacx, Isaacy)
+        //Make the reference point to Isaacx, Isaacy
+      rotate(radians(90))
+        //Rotate img and axis' by 90 degrees???
+        //Why it says radians, idk. But it works.
+      scale(1.0,-1.0); //Moves it 424/knifescale(5)
+      image(knife, 10, -80, knife.width/knifescale, knife.height/knifescale);
+        //moves it to look natural and scale it by 'knifescale'(5)
+      pop();
+      }
+    if (keyIsDown(83)) { //s
+      push();
+      imageMode(CENTER)
+      translate(Isaacx, Isaacy)
+      rotate(radians(180))
+      image(knife, 0, -80, knife.width/knifescale, knife.height/knifescale);
+      pop();
+      }
+    if (keyIsDown(68)) { //d
+      push();
+      imageMode(CENTER)
+      translate(Isaacx, Isaacy)
+      rotate(radians(90))
+      image(knife, 10, -80, knife.width/knifescale, knife.height/knifescale);
+      pop();
+      }
+    if (keyIsDown(87)) { //w
+      push();
+      imageMode(CENTER)
+      translate(Isaacx, Isaacy)
+      image(knife, 0, -80, knife.width/knifescale, knife.height/knifescale);
+      pop();
+      }
   }
