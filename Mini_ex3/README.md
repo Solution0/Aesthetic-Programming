34b62679afefa0ad7a7c201f48948b274cd9dfa3
Solution0 <36187931+Solution0@users.noreply.github.com>
2018-02-21 09:58:42 +0100

    Adding Mini_ex3
    Creating folders and files for Mini_ex3.

---
+++ Mini_ex3/README.md
@@ -0,0 +1,10 @@
+# Mini exercise 3
+
+![alt text](https://github.com/Solution0/Aesthetic-Programming/blob/Github-Desktop/Mini_ex2/Isaaccode.JPG?raw=true)
+*This is a screenshot of the program.
+
+**URL:** https://rawgit.com/Solution0/Aesthetic-Programming/Github-Desktop/Mini_ex2/empty-example/index.html
+- Using the arrow keys you control Isaac. W, A, S and D controls the knife in a given direction.
+
+# Title
+Placeholder text, description will come.
---
+++ Mini_ex3/empty-example/sketch.js
@@ -0,0 +1,19 @@
+/*Welcome to Mini_ex3. This exercises theme is Throbbers.*/
+
+function preload() {
+  }
+
+function windowResized() {
+  setup();
+  //Run setup once more, to accomodate new screen size
+  }
+
+function setup() {
+  createCanvas(windowWidth-5, windowHeight-5);
+  //Here I setup the canvas size. Its depended on the
+  //size of the window of the browser in which it is
+  //ran. Minus 5 so the scrollbars doesnt trigger
+  }
+
+function draw() {
+    }
