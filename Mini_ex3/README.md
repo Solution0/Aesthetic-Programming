7863ca43eb8ad383623e5d2ce1e2d8cfef6ace10
Solution0 <36187931+Solution0@users.noreply.github.com>
2018-02-26 22:31:55 +0100

    Update README

--- Mini_ex3/README.md
+++ Mini_ex3/README.md
@@ -1,10 +1,19 @@
 # Mini exercise 3
 
-![alt text](https://github.com/Solution0/Aesthetic-Programming/blob/Github-Desktop/Mini_ex2/Isaaccode.JPG?raw=true)
+![alt text](https://github.com/Solution0/Aesthetic-Programming/blob/Github-Desktop/Mini_ex3/Screenshot.JPG?raw=true)
 *This is a screenshot of the program.
 
-**URL:** https://rawgit.com/Solution0/Aesthetic-Programming/Github-Desktop/Mini_ex2/empty-example/index.html
-- Using the arrow keys you control Isaac. W, A, S and D controls the knife in a given direction.
+**URL:** https://rawgit.com/Solution0/Aesthetic-Programming/Github-Desktop/Mini_ex3/empty-example/index.html
+- The throbber turns around endlessly.. almost like it planning something horrible..
 
-# Title
-Placeholder text, description will come.
+# Throbber fun or...?
+This weeks exercise has been focused on temporality.
+
+Throbbers and loading animations is ironically a bigger part than ever, on our blazing fast websites. Like an ex it turns up in the most unfortunate of times and make the tiniest of hairs raise from our arms. What is wrong? Processes? Internet traffic? Errors? The current trend is to never let you know, placehold it with cute animations and hope that you will stick along for another load.
+
+In the late night hours the letters of 'Buffering' blends together and shows us what we real feel like we're doing: 'Suffering'. The throbbers is a replacement of loading bars and actually telling you what is going on. This makes the waiting feel timeless and distorted. I hope this will die out and bring back the transparency from older more simpler times of software and programs.
+
+# The short development
+As I thought about how to reflect and..
+
+(Alexander and other respondents. I will finish writing this Thursday morning.)
--- Mini_ex3/empty-example/sketch.js
+++ Mini_ex3/empty-example/sketch.js
@@ -1,59 +1,81 @@
-/*Welcome to Mini_ex3. This exercises theme is Throbbers.*/
+/*Welcome to Mini_ex3. This exercises theme is Throbbers.
+For an easier reading experience. Start by folding everything.
+Edit/folding/fold all (Upper left)*/
 
 var rotationSpeed = 0;
+  //Rotation speed of the throbber.
+  //Is defined more in function draw()
 var n = 0;
+  //Pulse direction: 0 outwards, 1 inwards
 var s = 30;
+  //Placement of circles that is off x and y axis
 var b = 60;
+  //Placement of circles on x and y axis
 var bmax = 100;
+  //max pulse distance
 var bmin = 60;
+  //minimum pulse distance; same as b.
 var speed = 0.7;
+  //Pulse speed
 
 function windowResized() {
   setup();
-  //Run setup once more, to accomodate new screen size
+  //If window is resized: run setup once more
+  //to accomodate new screen size
   }
 
 function setup() {
   createCanvas(windowWidth-5, windowHeight-5);
-  //Here I setup the canvas size. Its depended on the
-  //size of the window of the browser in which it is
-  //ran. Minus 5 so the scrollbars doesnt trigger
-  background(0);
-  frameRate(60)
+  /*Here I setup the canvas size. Its depended on the
+  size of the window of the browser in which it is
+  ran. Minus 5 so the scrollbars doesnt trigger*/
   }
 
 function draw() {
   background(0);
+    //background color black
   translate(windowWidth/2, windowHeight/2)
+    //Set the reference point to the middle of the window.
   rotationSpeed += 0.015
-  rotate(i);
-  cirkel(0,-b,185,95,210); //1
-  cirkel(0,b,80,204,38); //5 255,68,58);
-  cirkel(-b,0,35,288,285); //7 254,193,2);
-  cirkel(b,0,254,293,2); //3 255,208,2);
-  cirkel(-s,-s,55,43,228); //8 80,204,38);
-  cirkel(s,s,255,208,2); //4 19,154,244);
-  cirkel(-s,s,19,254,244); //6 35,188,185);
-  cirkel(s,-s,255,68,58); //2 55,43,228);
+    //0.015 is added to the rotationSpeed every frame
+  rotate(rotationSpeed);
+    //rotate every circle
+  circle(0,-b,185,95,210); //1
+  circle(s,-s,255,68,58); //2
+  circle(b,0,254,293,2); //3
+  circle(s,s,255,208,2); //4
+  circle(0,b,80,204,38); //5
+  circle(-s,s,19,254,244); //6
+  circle(-b,0,35,288,285); //7
+  circle(-s,-s,55,43,228); //8
+  circle(-s,s,19,254,244); //6
+  //All circles with the respective color codes and placements
 
   if(n == 0) {
     s+=speed
     b+=speed
+    //If pulse direction = 0, add to distance
   } else if (n==1) {
-    s=s-speed
-    b=b-speed
-  }
+    s-=speed
+    b-=speed
+    //If pulse direction = 1, decrease distance
+    }
 
-  if (b>=bmax) {
-  n=1
-} else if(b<=bmin) {
-  n=0
+    if (b>=bmax) {
+      n=1
+      //Change pulse direction when bmax is hit
+    } else if(b<=bmin) {
+      n=0
+      //Change pulse direction when bmin is hit
 }
 
 }
-function cirkel(x,y,r,g,b){
+function circle(x,y,r,g,b){
   noStroke();
   fill(r,g,b,150);
   ellipse(x,y,75,75);
-
+  /* function 'circle' with x,y as placements
+  and rgb as the chosen colors for the circle.
+  150 is given as the transparency value.
+  75 as the radius*/
 }
