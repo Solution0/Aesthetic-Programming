25af44d100947a123e418fa14a65c52b88ba138e
Solution0 <36187931+Solution0@users.noreply.github.com>
2018-03-14 12:50:54 +0100

    Failed sketch

--- Mini_ex5/empty-example/sketch.js
+++ Mini_ex5/empty-example/sketch.js
@@ -2,12 +2,38 @@
 data capturing, a very controversial thing in modern time.
 Start by 'fold all', Go to Edit/Folding/Fold all (Top left)*/
 
+var hand;
+var handImage;
+var aisle;
+var chips;
+
+function preload() {
+  handImage = loadImage('Assets/Hand.png');
+  aisle = loadImage('Assets/Aisle.png');
+
+}
+
 function windowResized() {
   setup();
   //If window is resized: run setup once more to accomodate new screen size
   }
 
 function setup() {
-  createCanvas(windowWidth-5, windowHeight-5);
-    /*Here I setup the canvas size. Its depended on the
+  createCanvas(900, 900);
+  /*Here I setup the canvas size. Its depended on the windows width and height*/
+  image(aisle, 0, 0)
+
+  chips = createSprite(100,100);
+  chips.addImage(loadImage('Assets/chips.png'));
+
+  }
+
+
+function draw() {
+  drawSprites();
+  hand = createSprite(mouseX, mouseY);
+  hand.addImage(handImage);
+
+  drawSprites();
+
 }
